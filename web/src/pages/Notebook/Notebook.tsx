dbe325aa599f5bdc6357cc2c7a1ecd32afaf772d
mayudev <99561108+mayudev@users.noreply.github.com>
2022-05-31 23:25:59 +0200

    feat(notebook): add password prompt to notebook page

--- web/src/pages/Notebook/Notebook.tsx
+++ web/src/pages/Notebook/Notebook.tsx
@@ -2,6 +2,7 @@ import { SerializedError } from '@reduxjs/toolkit'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import PasswordPrompt from '../../components/PasswordPrompt/PasswordPrompt'
 import Spinner from '../../components/Spinner/Spinner'
 import {
   fetchNotebook,
@@ -23,53 +24,66 @@ export default function Notebook() {
   const status = useAppSelector(state => state.notebook.status)
 
   const [errorMessage, setErrorMessage] = useState('')
+  const [showPasswordPrompt, setShowPasswordPrompt] = useState(false)
 
-  useEffect(() => {
-    async function fetchData(id: string) {
-      try {
-        await dispatch(
-          fetchNotebook({
-            id,
-            jwt: '', // TODO
-          })
-        ).unwrap()
-      } catch (e) {
-        const err = e as SerializedError
-        switch (err.code) {
-          case '401':
-            setErrorMessage('Not authorized.')
-            // show password prompt
-            break
-          case '404':
-            setErrorMessage('Notebook not found.')
-            break
-          case '500':
-            setErrorMessage('Internal server error.')
-            break
-          default:
-            setErrorMessage('An unknown error occurred.')
-            break
-        }
+  const fetchData = async (id: string, token: string) => {
+    try {
+      await dispatch(
+        fetchNotebook({
+          id,
+          jwt: token, // TODO
+        })
+      ).unwrap()
+    } catch (e) {
+      const err = e as SerializedError
+      switch (err.code) {
+        case '401':
+          setErrorMessage('Not authorized')
+          setShowPasswordPrompt(true)
+          break
+        case '404':
+          setErrorMessage('Notebook not found')
+          break
+        case '500':
+          setErrorMessage('Internal server error')
+          break
+        default:
+          setErrorMessage('An unknown error occurred')
+          break
       }
     }
-
+  }
+  useEffect(() => {
     // If current notebook isn't already present in state, fetch it.
     if (params.id !== notebook.id) {
-      fetchData(params.id!)
+      fetchData(params.id!, '')
     }
-  }, [dispatch, params.id, notebook.id])
+  }, [params.id, notebook.id])
+
+  const passwordEntered = async (success: boolean, token: string) => {
+    setShowPasswordPrompt(false)
+
+    if (!success) return
+
+    fetchData(params.id!, token)
+  }
 
   const display = () => {
     switch (status) {
       case 'failed':
+        if (showPasswordPrompt) {
+          return (
+            <PasswordPrompt notebook={params.id!} onSubmit={passwordEntered} />
+          )
+        }
         return <NotebookError>{errorMessage}</NotebookError>
+      case 'idle':
       case 'pending':
         return (
           <Center>
             <Spinner width={50} borderWidth={5} primary />
           </Center>
         )
-      case 'idle':
       case 'succeeded':
         return <div>name: {notebook.name}</div>
     }
