1c40d7b6b4c056d13e677a4ffe84f9665db07269
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-03 21:50:07 +0200

    feat(notebook): style add button

---
+++ web/src/pages/Notebook/Notes/Notes.styles.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import {
+  Accent,
+  BackgroundNavbar,
+  BackgroundSecondary,
+} from '../../../lib/colors'
+
+export const Container = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+
+  margin: 10px;
+`
+
+export const CreateNote = styled.button`
+  all: unset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 38px;
+
+  background: ${BackgroundSecondary};
+  border-radius: 6px;
+  border: 2px solid transparent;
+
+  transition: var(--transition-theme);
+  cursor: pointer;
+
+  &:hover {
+    border: 2px solid ${Accent};
+    color: ${Accent};
+  }
+`
--- web/src/pages/Notebook/Notes/Notes.tsx
+++ web/src/pages/Notebook/Notes/Notes.tsx
@@ -1,8 +1,11 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAppDispatch, useAppSelector } from '../../../app/hooks'
 import Button from '../../../components/Button/Button'
 import Note from '../../../components/Note/Note'
 import { selectNoteIds } from '../../../features/notebook/notebook.slice'
 import { noteCreate } from '../../../features/notebook/notebook.thunks'
+import { Container, CreateNote } from './Notes.styles'
 
 export default function Notes() {
   const dispatch = useAppDispatch()
@@ -13,12 +16,17 @@ export default function Notes() {
   }
 
   return (
-    <div>
-      <h1>notes</h1>
-      <Button onClick={() => create()}>Create new note</Button>
+    <Container>
+      <CreateNote
+        title="Create a new note"
+        aria-label="Create a new note"
+        onClick={() => create()}
+      >
+        <FontAwesomeIcon size="3x" icon={faPlus} />
+      </CreateNote>
       {noteIds.map(noteId => (
         <Note key={noteId} noteId={noteId} />
       ))}
-    </div>
+    </Container>
   )
 }
